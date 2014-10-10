d362bd41f912a6eef321bc78bfed53fa870b3047
Afay Zangetsu <afay.zangetsu@gmail.com>
2014-10-10 21:51:43 +0400

    few changes

--- init.el
+++ init.el
@@ -17,6 +17,10 @@
 (setq inhibit-startup-message t)
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(delete-selection-mode t)
  '(ergoemacs-ctl-c-or-ctl-x-delay 0.2)
  '(ergoemacs-handle-ctl-c-or-ctl-x (quote both))
@@ -27,13 +31,16 @@
  '(gnutls-min-prime-bits 1024)
  '(initial-buffer-choice t)
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(initial-scratch-message " ")
  '(org-CUA-compatible nil)
  '(shift-select-mode nil)
  '(smex-prompt-string "[Alt+A]")
  '(whitespace-style (quote (face lines-tail))))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(whitespace-empty ((t (:background "VioletRed1" :foreground "DeepPink4")))))
 
 (setq make-backup-files nil) ; Don't want any backup files
@@ -61,6 +68,12 @@
 (tramp-parse-shostkeys "/etc/ssh2/hostkeys/*")
 (tramp-parse-shostkeys "~/.ssh2/hostkeys/*")
 
+;; =========================== Browser ==================================
+
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "firefox-nightly")
+(global-set-key (kbd "C-x g") 'ergoemacs-lookup-google) 
+
 ;; =========================== Org mode  ==================================
 
 (add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp" t)
@@ -75,6 +88,14 @@
 
 ;; =========================== Features  ==================================
 
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(setq ac-auto-show-menu 0.5)
+(setq ac-menu-height 20)
+(global-auto-complete-mode 1)
+
 (require 'linum+)
 (setq linum-format "%d ")
 (global-linum-mode 1)
@@ -82,6 +103,14 @@
 (require 'ido-hacks)
 (ido-mode t)
 (setq ido-enable-flex-matching t)
+
+;; (add-to-list 'load-path "~/emacs.d/flx")
+;; (require 'flx-ido)
+;; (ido-mode 1)
+;; (ido-everywhere 1)
+;; (flx-ido-mode 1)
+;; ;; disable ido faces to see flx highlights.
+;; (setq ido-use-faces nil)
 
 (add-to-list 'load-path "~/.emacs.d/multiple-cursors")
 (require 'multiple-cursors)
@@ -93,11 +122,56 @@
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
 
+;; =========================== Sudo  ======================================
+
+(defun sudo-before-save-hook ()
+  (set (make-local-variable 'sudo:file) (buffer-file-name))
+  (when sudo:file
+    (unless(file-writable-p sudo:file)
+      (set (make-local-variable 'sudo:old-owner-uid) (nth 2 (file-attributes sudo:file)))
+      (when (numberp sudo:old-owner-uid)
+	(unless (= (user-uid) sudo:old-owner-uid)
+	  (when (y-or-n-p
+		 (format "File %s is owned by %s, save it with sudo? "
+			 (file-name-nondirectory sudo:file)
+			 (user-login-name sudo:old-owner-uid)))
+	    (sudo-chown-file (int-to-string (user-uid)) (sudo-quoting sudo:file))
+	    (add-hook 'after-save-hook
+		      (lambda ()
+			(sudo-chown-file (int-to-string sudo:old-owner-uid)
+					 (sudo-quoting sudo:file))
+			(if sudo-clear-password-always
+			    (sudo-kill-password-timeout)))
+		      nil   ;; not append
+		      t	    ;; buffer local hook
+		      )))))))
+
+(add-hook 'before-save-hook 'sudo-before-save-hook)
+
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(global-set-key (kbd "C-x w") 'sudo-edit)
+
 ;; =========================== Commands  ==================================
 
 (require 'smex)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 ;; =========================== Keybinding  ==================================
 
@@ -158,20 +232,29 @@
         "~/.emacs.d/yasnippet/yasmate/snippets" ;; the yasmate collection
         "~/.emacs.d/yasnippet/snippets"))
 
-;; =========================== Commands  ==================================
+;; =========================== Packages  ==================================
 
-(require 'smex)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (package-initialize)
+
+;; (add-to-list 'load-path "~/.emacs.d/el-get")
+
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+;;     (goto-char (point-max))
+;;     (eval-print-last-sexp)))
+
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/recipes")
+;; (el-get 'sync)
 
 ;; =========================== Rails  ==================================
 
 (require 'rvm)
 (rvm-use-default)
-
-(add-to-list 'load-path "~/.emacs.d/robe")
-(add-hook 'ruby-mode-hook 'robe-mode)
-(add-hook 'robe-mode-hook 'ac-robe-setup)
 
 (add-to-list 'load-path "~/.emacs.d/enhanced-ruby-mode")
 (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
@@ -179,6 +262,88 @@
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 (setq enh-ruby-program "~/.rvm/rubies/ruby-1.9.3-p547/bin/ruby")
+
+;; =========================== Ruby  ====================================
+
+;; (require 'flymake)
+
+;; ;; I don't like the default colors :)
+;; (set-face-background 'flymake-errline "red4")
+;; (set-face-background 'flymake-warnline "dark slate blue")
+
+;; ;; Invoke ruby with '-c' to get syntax checking
+;; (defun flymake-ruby-init ()
+;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;; 	 (local-file  (file-relative-name
+;;                        temp-file
+;;                        (file-name-directory buffer-file-name))))
+;;     (list "ruby" (list "-c" local-file))))
+
+;; (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+;; (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+
+;; (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+;; (add-hook 'ruby-mode-hook
+;;           '(lambda ()
+
+;; 	     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+;; 	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+;; 		 (flymake-mode))
+;; 	     ))
+
+;; (add-hook 'web-mode-hook
+;;           (lambda () (flyspell-prog-mode)))
+;; ;; flyspell mode breaks auto-complete mode without this.
+;; (ac-flyspell-workaround)
+
+(require 'flyspell)
+(setq flyspell-issue-message-flg nil)
+(add-hook 'enh-ruby-mode-hook
+          (lambda () (flyspell-prog-mode)))
+
+(add-hook 'ruby-mode-hook 'projectile-mode)
+(setq projectile-enable-caching t)
+
+(setq-default ispell-program-name "aspell")
+(setq ispell-local-dictionary "russian")
+(global-flyspell-mode)
+
+(require-package 'ruby-mode)
+(require-package 'ruby-hash-syntax)
+(add-auto-mode 'ruby-mode
+               "Rakefile\\'" "\\.rake\\'" "\\.rxml\\'"
+               "\\.rjs\\'" "\\.irbrc\\'" "\\.pryrc\\'" "\\.builder\\'" "\\.ru\\'"
+               "\\.gemspec\\'" "Gemfile\\'")
+
+;; (add-to-list 'load-path "~/.emacs.d/robe")
+;; (require-package 'robe)
+;; ; для работы с rvm
+;; (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+;;    (rvm-activate-corresponding-ruby))
+
+;; (after-load 'ruby-mode
+;;   (add-hook 'ruby-mode-hook 'robe-mode))
+;; (after-load 'robe
+;;   (add-hook 'robe-mode-hook 'ac-robe-setup))
+;;   (add-hook 'robe-mode-hook
+;;             (lambda ()
+;;               (add-to-list 'ac-sources 'ac-source-robe)
+;;               (set-auto-complete-as-completion-at-point-function)))
+
+;; (setq rsense-home "$RSENSE_HOME")
+;; (add-to-list 'load-path (concat rsense-home "/etc"))
+;; (require 'rsense)
+
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-c c") 'ac-complete-rsense)))
+
+;; (add-hook 'ruby-mode-hook
+;;          (lambda ()
+;;            (add-to-list 'ac-sources 'ac-source-rsense-method)
+;;            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
 
 ;; =========================== Rinary  ==================================
 
@@ -260,10 +425,15 @@
 
 ;; =========================== Templates...  ==================================
 
-(require 'slim-mode)
+(autoload 'scss-mode "scss-mode")
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(require 'css-mode)
+(require 'sass-mode)
 
-(add-to-list 'load-path "~/.emacs.d/js2-mode")
-(require 'js2-mode)
+(require 'rainbow-mode)
+(global-rainbow-mode)
+
+(require 'slim-mode)
 
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist
