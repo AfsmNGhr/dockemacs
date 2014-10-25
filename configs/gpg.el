b5ace62a1baedc3545188b3729f7552284fb6b2c
Afay Zangetsu <afay.zangetsu@gmail.com>
2014-10-25 17:26:44 +0400

    back to old config format...

--- configs/gpg.el
+++
@@ -1,7 +0,0 @@
-;; =========================== GPG ===================================
-
-(require 'epa-file)
-(epa-file-enable)
-(setq epa-file-cache-passphrase-for-symmetric-encryption t)
-
-;; ====================== To be continued... =========================
