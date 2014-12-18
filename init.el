dd01ce6491ca08fad965fdc6737e993d2493a0dd
Afay Zangetsu <afay.zangetsu@gmail.com>
2014-12-18 12:15:40 +0300

    fix configs/ruby ...

--- init.el
+++ init.el
@@ -8,10 +8,10 @@
 (load "~/.emacs.d/configs/commands.el")
 (load "~/.emacs.d/configs/kbd.el")
 (load "~/.emacs.d/configs/snippets.el")
-(load "~/.emacs.d/configs/ruby.el")
 (load "~/.emacs.d/configs/js.el")
 (load "~/.emacs.d/configs/clojure.el")
-(load "~/.emacs.d/configs/rinari.el")
+(load "~/.emacs.d/configs/ruby/ruby.el")
+(load "~/.emacs.d/configs/ruby/rinari.el")
 (load "~/.emacs.d/configs/templates.el")
 (load "~/.emacs.d/configs/server.el")
 
