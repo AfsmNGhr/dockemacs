312d0998a754f626497f16cff6b4bd8e731ae301
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-04-06 21:40:52 +0300

    delete gpg ...

--- conf.d/term.el
+++ conf.d/term.el
@@ -2,17 +2,8 @@
 
 (use-package multi-term :defer t :ensure t
   :config
-  (setq multi-term-program "/bin/bash"
+  (setq multi-term-program "/bin/sh"
         comint-input-ring-file-name (getenv "HISTFILE"))
   (comint-read-input-ring t))
-
-;; Do not use gpg agent when runing in terminal
-(defadvice epg--start (around advice-epg-disable-agent activate)
-  (let ((agent (getenv "GPG_AGENT_INFO")))
-    (when (not (display-graphic-p))
-      (setenv "GPG_AGENT_INFO" nil))
-    ad-do-it
-    (when (not (display-graphic-p))
-      (setenv "GPG_AGENT_INFO" agent))))
 
 ;; ========================= To be continued... ================================
