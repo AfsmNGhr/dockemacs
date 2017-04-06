7a5d3bd9b7257b9ca778ed0e8984931ac31ca376
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-04-06 20:20:06 +0300

    fix: gpg without gui ...

--- conf.d/term.el
+++ conf.d/term.el
@@ -1,16 +1,18 @@
 ;; ================================ Shell ======================================
 
-(use-package pinetry :defer t :ensure t
-  :config (allow-emacs-pinentry))
-
 (use-package multi-term :defer t :ensure t
   :config
   (setq multi-term-program "/bin/bash"
         comint-input-ring-file-name (getenv "HISTFILE"))
   (comint-read-input-ring t))
 
-;; (unless (display-graphic-p)
-;;   (use-package xclip :ensure t
-;;     :config (xclip-mode 1)))
+;; Do not use gpg agent when runing in terminal
+(defadvice epg--start (around advice-epg-disable-agent activate)
+  (let ((agent (getenv "GPG_AGENT_INFO")))
+    (when (not (display-graphic-p))
+      (setenv "GPG_AGENT_INFO" nil))
+    ad-do-it
+    (when (not (display-graphic-p))
+      (setenv "GPG_AGENT_INFO" agent))))
 
 ;; ========================= To be continued... ================================
