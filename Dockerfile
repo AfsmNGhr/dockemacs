f60227582f7046c1c8242e1faec8936c8c3cdc98
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-05-15 20:07:10 +0300

    test: xref-js2 with ag ...

--- Dockerfile
+++ Dockerfile
@@ -19,7 +19,8 @@ RUN apk update && \
         --repository http://dl-cdn.alpinelinux.org/alpine/edge/main/ \
         --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ \
         --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
-        bash sudo emacs-nox xterm docker global git openssh gnupg nodejs-npm && \
+        bash sudo emacs-nox xterm docker global the_silver_searcher \
+        git openssh gnupg nodejs-npm && \
     rm -rf /usr/share/man /tmp/* /var/cache/apk/* /var/log/* /root/.cache
 
 COPY sbin/* /usr/local/sbin/
--- conf.d/js.el
+++ conf.d/js.el
@@ -13,11 +13,26 @@
                         js2-highlight-level 3
                         js2-global-externs '("angular")
                         js2-indent-on-enter-key t)
+  (setq flycheck-disabled-checkers '(javascript-jshint)
+        flycheck-checkers '(javascript-eslint))
   (add-to-list 'js2-mode-hook 'flycheck-mode))
 
 (use-package tern :ensure t :defer t
   :config (add-hook 'javascript-hook 'tern-mode)
   (add-to-list 'company-backends 'company-tern))
+
+(use-package js2-refactor :ensure t :defer t
+  :config
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill))
+
+(use-package xref-js2 :ensure t :defer t
+  :config
+  (define-key js-mode-map (kbd "M-.") nil)
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
 (defun reset-eslint-rc ()
   (let ((rc-path (if (projectile-project-p)
