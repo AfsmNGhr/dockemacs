962791c95a04436430f9c71be7b71a67920b652e
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-05-15 20:29:41 +0300

    ref: loading modes ...

--- conf.d/js.el
+++ conf.d/js.el
@@ -13,26 +13,28 @@
                         js2-highlight-level 3
                         js2-global-externs '("angular")
                         js2-indent-on-enter-key t)
-  (setq flycheck-disabled-checkers '(javascript-jshint)
-        flycheck-checkers '(javascript-eslint))
-  (add-to-list 'js2-mode-hook 'flycheck-mode))
+  (setq flycheck-disabled-checkers '(javascript-jshint))
+  (add-to-list 'js2-mode-hook 'flycheck-mode)
+  (add-to-list 'js2-mode-hook 'tern-mode)
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
 (use-package tern :ensure t :defer t
-  :config (add-hook 'javascript-hook 'tern-mode)
-  (add-to-list 'company-backends 'company-tern))
+  :config
+  (add-to-list 'company-backends 'company-tern)
+  (define-key tern-mode-keymap (kbd "M-.") nil)
+  (define-key tern-mode-keymap (kbd "M-,") nil))
 
 (use-package js2-refactor :ensure t :defer t
   :config
-  (add-hook 'js2-mode-hook #'js2-refactor-mode)
   (js2r-add-keybindings-with-prefix "C-c C-r")
   (define-key js2-mode-map (kbd "C-k") #'js2r-kill))
 
 (use-package xref-js2 :ensure t :defer t
   :config
-  (define-key js-mode-map (kbd "M-.") nil)
-  (add-hook 'js2-mode-hook
-            (lambda ()
-              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
+  (define-key js-mode-map (kbd "M-.") nil))
 
 (defun reset-eslint-rc ()
   (let ((rc-path (if (projectile-project-p)
