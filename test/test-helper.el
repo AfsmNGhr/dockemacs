cacb58d5f4adbc8f9796a67e250653947f00777c
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-08-25 22:16:07 +0300

    fix: pattern ert ...

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,4 +1,4 @@
-(when (require 'undercover nil t)
-  (undercover "init.el"))
+(require 'undecover)
 
+(undercover "*.el" "conf.d/*.el" (:exclude "*-test.el"))
 (message "Running tests on Emacs %s" emacs-version)
