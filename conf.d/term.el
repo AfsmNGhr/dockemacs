986bf3ed932067b10fba208f7850ebdaca92aa66
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-11-23 22:09:07 +0300

    fix: delete old configs ... [ci skip]

--- conf.d/term.el
+++
@@ -1,9 +0,0 @@
-;; ================================ Shell ======================================
-
-(use-package multi-term :defer t :ensure t
-  :config
-  (setq multi-term-program "/bin/sh"
-        comint-input-ring-file-name (getenv "HISTFILE"))
-  (comint-read-input-ring t))
-
-;; ========================= To be continued... ================================
