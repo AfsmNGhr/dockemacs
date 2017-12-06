da5cb9e933231911e8a4c0c9207a378f06adcd78
Afay Zangetsu <afay.zangetsu@gmail.com>
2017-12-06 21:42:49 +0300

    fix: tests & export vars ...

--- tests/test-interface.el
+++ tests/test-interface.el
@@ -1,6 +1,8 @@
 (require 'init.el)
 (require 'undercover-init.el)
 
-(describe "defun: `my/initialize'"
-  (it "default params"
-      (expect (princ default-tab-width) :to-be 2)))
+(describe "Indentation"
+  (it "tab offset"
+      (expect (princ default-tab-width) :to-be 2))
+  (it "space indentation"
+      (expect (princ indett-tabs-mode) :to-be nil)))
