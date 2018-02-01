ad25a14a6c0bdf481508d6acae3003f18787d27f
Afay Zangetsu <afay.zangetsu@gmail.com>
2018-02-01 19:33:42 +0300

    fix: add gc ...

--- tests/test-interface.el
+++ tests/test-interface.el
@@ -2,7 +2,15 @@
 (require 'undercover-init.el)
 
 (describe "Indentation"
-  (it "tab offset"
-      (expect (princ default-tab-width) :to-be 2))
-  (it "space indentation"
-      (expect (princ indett-tabs-mode) :to-be nil)))
+          (it "tab offset"
+              (expect (princ default-tab-width) :to-be 2))
+          (it "always indent"
+              (expect (princ tab-always-indent) :to-be 'complete))
+          (it "space indentation"
+              (expect (princ indent-tabs-mode) :to-be nil)))
+
+(describe "Garbage Collector"
+          (it "treshold"
+              (expect (princ gc-cons-threshold) :to-be 800000))
+          (it "percentage"
+              (except (princ gc-cons-percentage) :to-be 0.1)))
--- tests/undercover-init.el
+++ tests/undercover-init.el
@@ -6,7 +6,7 @@
 (let ((debug-on-error t)
       (user-init-file "./init.el")
       (user-emacs-directory default-directory)
-      (load-path (delq default-directory load-path)))
+      ;; (load-path (delq default-directory load-path)))
   (load-file user-init-file)
   (run-hooks 'after-init-hook))
 
