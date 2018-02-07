d14f849d0d5a788d87e12ad3f5a810e6a1504fc3
Afay Zangetsu <afay.zangetsu@gmail.com>
2018-02-07 20:41:31 +0300

    fix: add test ...

---
+++ tests/test-initialize.el
@@ -0,0 +1,11 @@
+(require 'undercover-init.el)
+
+(describe "Garbage Collector on startup"
+          (it "treshold"
+              (expect (princ gc-cons-threshold) :to-be most-positive-fixnum))
+          (it "percentage"
+              (expect (princ gc-cons-percentage) :to-be 0.6)))
+
+(describe "File name handler"
+          (it "alist"
+              (expect (princ file-name-handler-alist) :to-be nil)))
